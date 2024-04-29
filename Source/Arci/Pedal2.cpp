70c6878f708fdd4406af7d7343a48652d32bc230
Babe-to <168372122+Babe-to@users.noreply.github.com>
2024-04-29 17:08:06 +0300

    Du

---
+++ Source/Arci/Pedal2.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pedal2.h"
+
+Pedal2::Pedal2()
+{
+}
+
+Pedal2::~Pedal2()
+{
+}
---
+++ Source/Arci/Pedal2.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class ARCI_API Pedal2
+{
+public:
+	Pedal2();
+	~Pedal2();
+};
